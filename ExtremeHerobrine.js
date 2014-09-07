7f2413840042a431fcc95cb117b1bbad0b3657e0
bagas123 <bagaskara822@gmail.com>
2014-09-07 17:31:16 +0700

    Update ExtremeHerobrine.js

--- ExtremeHerobrine.js
+++ ExtremeHerobrine.js
@@ -1,5 +1,10 @@
 //Scripted by bagas
 
+//*global variables*//
+var version = "4.5";
+var newVersion;
+var ctx = com.mojang.minecraftpe.MainActivity.currentMainActivity.get();
+
 //*items,texture*//
  ModPE.overrideTexture("images/herobrineextreme.png", "https://dl.dropbox.com/s/1fba5tsvzc03qgk/skin_20140626100232131481.png");
 ModPE.setItem(454,"apple_golden", 0, "Spawner of Herobrine");
@@ -443,4 +448,126 @@ Level.spawnMob(Entity.getX(v), Entity.getY(v) ,Entity.getZ(v)+1,32,"mob/zombie.p
 
 }
 }
+function showBind() {
+	ctx.runOnUiThread(new java.lang.Runnable( { run: function () {
+		try {
+			var btn = new android.widget.Button(ctx);
+			btn.setText(bindCommand[0].toUpperCase());
+			btn.setTextColor(android.graphics.Color.CYAN);
+			btn.setOnClickListener(new android.view.View.OnClickListener() {
+				onClick: function () {
+					main(bindCommand);
+				}
+			});
+			bindBtn = new android.widget.PopupWindow(btn, android.view.ViewGroup.LayoutParams.WRAP_CONTENT, android.view.ViewGroup.LayoutParams.WRAP_CONTENT);
+			if(bindLft) {
+				bindBtn.showAtLocation(ctx.getWindow().getDecorView(), android.view.Gravity.BOTTOM | android.view.Gravity.LEFT, dip2px(ctx, 85), dip2px(ctx, 85));
+			}
+			else {
+				bindBtn.showAtLocation(ctx.getWindow().getDecorView(), android.view.Gravity.BOTTOM | android.view.Gravity.RIGHT, dip2px(ctx, 85), dip2px(ctx, 85));
+			}
+		} catch(e) {
+			errorMsg("Line 1110: " + e);
+		}
+	}}));
+}
 
+function dismissBind() {
+	ctx.runOnUiThread(new java.lang.Runnable({ run: function () {
+		if (bindBtn != null) {
+			bindBtn.dismiss();
+		}
+	}}));
+	bindCommand = [];
+}
+
+function compareVersion() {
+	var r  = new java.lang.Runnable({ run: function() {
+		try {
+			var u = new java.net.URL("https://raw.githubusercontent.com/bagas123/Mod-Creations/master/version");
+			var c = u.openConnection();
+			c.setRequestMethod("GET");
+			c.setDoOutput(true);
+			c.connect();
+			c.getContentLength();
+			var input = c.getInputStream();
+			var contents = java.lang.reflect.Array.newInstance(java.lang.Byte.TYPE, 1024);
+			var bytesRead = 0; 
+			var strFileContents;
+			while((bytesRead = input.read(contents)) != -1) { 
+				strFileContents = new java.lang.String(contents, 0, bytesRead);			   
+			}
+			newVersion = strFileContents;
+			if(version+"\n" != strFileContents) {
+				colourMsg("New version! " + newVersion);
+				showDownloadAlert = true;
+			}
+			
+		}
+		catch(e) {
+			if(e == "JavaException: java.net.UnknownHostException: Unable to resolve host \"raw.githubusercontent.com\": No address associated with hostname") { }
+			else {
+				errorMsg("Hmm.." + e);
+			}
+		}
+	}});
+	var th = new java.lang.Thread(r);
+	th.start();
+}
+
+function updateScript() {
+	try {
+		var builder = new android.app.AlertDialog.Builder(ctx);
+		builder.setTitle("New version available!");
+		builder.setMessage("An update to Extreme Herobrine Mod was found!\nWould you like to download it now?\nCurrent version: " + version + "\nNew version: " + newVersion);
+		builder.setNegativeButton("No", new android.content.DialogInterface.OnClickListener() {
+			onClick: function(par1) {
+				dialog.dismiss();
+			}
+		});
+		builder.setPositiveButton("Yes", new android.content.DialogInterface.OnClickListener() {
+			onClick: function(par1) {
+				var ru  = new java.lang.Runnable({ run: function() {
+					try {
+						var u = new java.net.URL("https://raw.githubusercontent.com/bagas123/Mod-Creations/master/ExtremeHerobrine.js");
+						var c = u.openConnection();
+						c.setRequestMethod("GET");
+						c.setDoOutput(true);
+						c.connect();
+						c.getContentLength();
+						var input = c.getInputStream();
+						var contents = java.lang.reflect.Array.newInstance(java.lang.Byte.TYPE, 1024);
+						var bytesRead = 0;
+						while((bytesRead = input.read(contents)) != -1) { 
+							newScript += new java.lang.String(contents, 0, bytesRead);			   
+						}
+						var patchesFolder = ctx.getDir("modscripts", 0);
+						var scriptFile = new java.io.File(patchesFolder, "ExtremeHerobrine.js");
+						var printWriter = new java.io.PrintWriter(scriptFile);
+						printWriter.write(newScript);
+						printWriter.flush();
+						printWriter.close();
+						try {
+							net.zhuoweizhang.mcpelauncher.ScriptManager.setEnabled(scriptFile, false);
+							net.zhuoweizhang.mcpelauncher.ScriptManager.setEnabled(scriptFile, true);
+							colourMsg("Downloaded and enabled!");
+						}
+						catch(e) {
+							errorMsg("Line 451: " + e);
+						}
+					}
+					catch(e) {
+						errorMsg("Line 466: " + e);
+					}
+				}});
+				var th = new java.lang.Thread(ru);
+				th.start();
+			}
+		});
+		var dialog = builder.create();
+		dialog.show()
+	}
+	catch(e) {
+		errorMsg("Line 484: " + e);
+	}
+}
